2a1c2f8b755714a592ea5ec5704c78a11689b2de
LJY981008 <88678179+LJY981008@users.noreply.github.com>
2025-04-24 10:46:07 +0900

    Feat(Lv3) : Kiosk 기능작성
    start, printMenu, printSelect 메서드 작성

--- src/lv3/Kiosk.java
+++ src/lv3/Kiosk.java
@@ -1,9 +1,6 @@
 package lv3;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Kiosk {
     private List<MenuItem> menuItems = new ArrayList<>();
@@ -11,6 +8,42 @@ public class Kiosk {
         this.menuItems.addAll(Arrays.asList(items));
     }
     public void start(){
+        Scanner sc = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("0")){
+            printMenu();
+            try {
+                input = sc.nextLine();
+                int index = Integer.parseInt(input) - 1;
+                if (index == -1) {
+                    System.out.println("프로그램을 종료합니다.");
+                } else if (index > menuItems.size() || index < -1) {
+                    throw new InputMismatchException();
+                }else{
+                    printSelect(index);
+                }
+            }catch (InputMismatchException e){
+                System.out.println("잘못된 입력입니다. 다시 입력해주세요.");
+            }catch (Exception e){
+                System.out.println("알수없는 오류 발생");
+            }
+        }
+    }
+    /**
+     * 전체 메뉴 출력
+     */
+    public void printMenu(s){
+        for(int i = 0; i < menuItems.size(); i++){
+            String[] menuItem = menuItems.get(i).getMenu();    // index = 0 메뉴이름, 1 가격, 2 설명
+            System.out.println(i+1 + ". " + menuItem[0] + "\t| W " +  menuItem[1] + "\t | " + menuItem[2]);
+        }
+    }
 
+    /**
+     * 선택한 메뉴 출력
+     */
+    public void printSelect(int index){
+        String[] menu = menuItems.get(index).getMenu();     // index = 0 메뉴이름, 1 가격
+        System.out.println("선택한 메뉴 : " + menu[0] + ", 가격 : " + menu[1]);
     }
 }
